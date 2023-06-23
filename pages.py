af4ecace6a07f6fe1b0d2f4e8606e1373233a478
gandalf-the-lonesome <abin.xv@gmail.com>
2023-06-23 21:12:17 +0530

    search for multiple keywords + design update

--- pages.py
+++ pages.py
@@ -15,21 +15,21 @@ def index():
     if request.method == "POST":
         # Get the uploaded PDF file and search term from the form
         file = request.files['input_file']
-        search_term = request.form['search_key']
+        search_terms = list(map(str.strip, request.form['search_key'].split(',')))
 
         # Save the uploaded PDF file
         input_pdf = os.path.join(current_app.root_path, f"uploads{os.path.sep}{secure_filename(file.filename)}")
         file.save(input_pdf)
 
         # Process the PDF file and generate the modified PDF
-        output_pdf = process_pdf(input_pdf, search_term)
+        output_pdf = process_pdf(input_pdf, search_terms)
 
         # Return the modified PDF as a response
         return Response(output_pdf, mimetype="application/pdf", headers={"Content-Disposition": f"attachment; filename={file.filename.replace('.pdf', '')}-processed.pdf"})
 
     return render_template("pages.html")
 
-def process_pdf(input_pdf, search_term):
+def process_pdf(input_pdf, search_terms):
     # extract tables
     tables = camelot.read_pdf(input_pdf, pages='all', line_scale=40)
 
@@ -37,7 +37,7 @@ def process_pdf(input_pdf, search_term):
     for table in tables[1:-2]:
         table.df = table.df.replace(r'\n', ' ', regex=True)
         for index in reversed(range(2, table.df.shape[0])):
-            if search_term not in table.df.iloc[index].to_string():
+            if not any(search_term in table.df.iloc[index].to_string() for search_term in search_terms):
                 table.df.drop(index, inplace=True)
 
     # Convert dfs into html tables
@@ -72,7 +72,8 @@ def process_pdf(input_pdf, search_term):
     combined_html += "<center></body></html>"
 
     # Highlight search text
-    combined_html = combined_html.replace(search_term, f"<span class='highlight'>{search_term}</span>")
+    for search_term in search_terms:
+        combined_html = combined_html.replace(search_term, f"<span class='highlight'>{search_term}</span>")
 
     # Add table header
     table_th = """
